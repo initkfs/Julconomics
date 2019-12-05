665a0def11c4a14b30960af821749be7cde483db
initkfs <none@none>
2019-12-05 03:23:24 +0300

    Add text report files

---
+++ enterprise/market/chart/TextReportGeneratorTest.jl
@@ -0,0 +1,41 @@
+#!/usr/bin/env julia
+# Author: initkfs 2019
+#
+module TextReportGeneratorTest
+
+using ExcelReaders, HTTP, DataFrames, UnicodePlots
+
+function run()
+
+    #WARNING!
+    #The data file is not included in the repository for licensing reasons.
+    #see https://data.gov.ru/opendata/7708523530-grainexport
+    dataFile = "$(@__DIR__)/data.xlsx"
+    @assert isfile(dataFile)
+
+    excelData = readxlsheet(dataFile, "Лист1", skipstartrows=1, nrows=20)
+    @assert length(excelData) > 0
+
+    dataTable = Base.convert(DataFrame, excelData)
+
+    chartX = String[]
+    chartY = Float64[]
+    for row in DataFrames.eachrow(dataTable)
+        push!(chartX, string(row.x1))
+        push!(chartY, float(row.x2))
+    end
+
+    @assert length(chartX) == length(chartY)
+    
+    plotResult = UnicodePlots.barplot(chartX, chartY)
+    
+    outFile = "$(@__DIR__)/out.txt"
+    stringResult = string(plotResult)
+    open(outFile, "w") do io
+        write(io, stringResult)
+    end;
+end
+
+@time run()
+
+end
