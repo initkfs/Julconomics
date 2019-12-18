b1059e8e9bc56f2333fd2d01341141010b29be5d
initkfs <none@none>
2019-12-19 00:12:49 +0300

    Fix, rename some variables

--- market/dataframe/ContentMorphologicalGenenator.jl
+++ market/dataframe/ContentMorphologicalGenenator.jl
@@ -7,10 +7,10 @@ using DataFrames
 
 function run(contentForm::DataFrames.AbstractDataFrame, contentTypes::DataFrames.AbstractDataFrame)::DataFrames.AbstractDataFrame
 
-    formCopy = copy(contentForm)
-    typesCopy = copy(contentTypes)
+    contentFormCopy = copy(contentForm)
+    contentTypesCopy = copy(contentTypes)
 
-    formAndTypeTable = DataFrames.join(sort!(contentForm), sort!(contentTypes), kind = :cross)
+    formAndTypeTable = DataFrames.join(sort!(contentFormCopy), sort!(contentTypesCopy), kind = :cross)
     
     return formAndTypeTable
 
